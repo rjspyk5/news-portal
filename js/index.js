6433b36c87e03e51527a97bd137cdff0a9d20e02
rjspyk5 <rjspyk5@gmail.com>
2024-03-02 16:46:01 +0600

    fetch catagories data when someone click on catagories

--- js/index.js
+++ js/index.js
@@ -1,26 +1,59 @@
-const loadData=async (url)=>{
-    const promisedObject=await fetch(url);
-    const allData=await promisedObject.json();
-    const catagories=allData.data.news_category
-   makeCatagoriesSection(catagories);
+//Create Catagory Section
+const catagorySection = document.getElementById("catagorySection");
+const makeCatagoriesSection = (datas) => {
+  const data = datas.data.news_category;
+  data.forEach((datum) => {
+    const span = document.createElement("span");
+    span.innerHTML = `<button onclick="handleButton(id)"  id=${datum.category_id} class="btn btn-ghost focus:bg-blue-600 focus:text-white text-black">${datum.category_name}</button> `;
+    catagorySection.appendChild(span);
+  });
+};
 
-}
+// Shown data when someone click on catagory
+const newsContainer = document.getElementById("news-container");
+const showDataWithCatagory = (datas) => {
+  const data = datas.data;
+  data.forEach((el) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+  <div class="card lg:card-side bg-base-100 shadow-xl">
+  <figure><img class="w-96" src=${el?.image_url} alt="Album"/></figure>
+  <div class="card-body">
+    <h2 class="card-title">New album is released!</h2>
+    <p>Click the button to listen on Spotiwhy app.</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Listen</button>
+    </div>
+  </div>
+</div> 
+  `;
+    newsContainer.appendChild(div);
+  });
 
-const catagorySection=document.getElementById("catagorySection")
-const makeCatagoriesSection=(data)=>{
-    data.forEach(datum => {
-        const span=document.createElement("span");
-        span.innerHTML=`
-        <button id=${
-            datum.category_id
-            } class="btn btn-ghost text-black">${datum.category_name
-            }</button>
-        `;
-       catagorySection.appendChild(span);
+  console.log(data);
+};
+// Clear previous
+const clearPrev = () => {
+  newsContainer.innerHTML = "";
+};
 
-        console.log(datum);
-    });
-}
+// Data fetching
+const loadData = async (url, fun) => {
+  const promisedObject = await fetch(url);
+  const allData = await promisedObject.json();
+  fun(allData);
+};
 
-
-loadData("https://openapi.programming-hero.com/api/news/categories");
+// handle when someone click on catagories
+const handleButton = (id) => {
+  clearPrev();
+  loadData(
+    `https://openapi.programming-hero.com/api/news/category/${id}`,
+    showDataWithCatagory
+  );
+};
+// Default call
+loadData(
+  "https://openapi.programming-hero.com/api/news/categories",
+  makeCatagoriesSection
+);
