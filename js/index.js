68e0a77aa33ecfe624df26314bfdb4fb87147f99
rjspyk5 <rjspyk5@gmail.com>
2024-03-02 18:59:03 +0600

    star and eye added on card

--- js/index.js
+++ js/index.js
@@ -15,24 +15,44 @@ const makeCatagoriesSection = (datas) => {
 const newsContainer = document.getElementById("news-container");
 const showDataWithCatagory = (datas) => {
   const data = datas.data;
-  data.forEach((el) => {
-    console.log(el);
-    const div = document.createElement("div");
-    div.innerHTML = `
+  console.log(data[0]);
+  data.length > 0
+    ? data.forEach((el) => {
+        const div = document.createElement("div");
+        div.innerHTML = `
   <div class="card py-5 lg:card-side bg-base-100 shadow-xl">
   <img class="w-96" src=${el?.image_url} alt="Album"/>
   <div class="px-8" >
     <h2 class="card-title">${el.title}</h2>
     <p>${el.details}</p>
     
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Listen</button>
+    <div class="flex justify-between"><div className="flex space-x-2"></div>
+    <div className="flex space-x-2">
+    <p>${el.total_view}</p>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+    </svg>
     </div>
+    
+    </div>
+    <div className="flex space-x-2">
+    <div class="rating">
+    <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+    <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+    <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+    <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+    <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+  </div>
+   
+    </div>
+    
   </div>
 </div> 
   `;
-    newsContainer.appendChild(div);
-  });
+        newsContainer.appendChild(div);
+      })
+    : (newsContainer.innerHTML = `no news available`);
 };
 // Clear previous
 const clearPrev = () => {
